ed53d5d155b39d2e7726e03e12bccc43821ea4b0
Mikhail Zelenkin <mzelenkin@gmail.com>
2020-02-19 14:33:41 +0300

    Api interface

--- api.go
+++ api.go
@@ -4,4 +4,6 @@ import "context"
 
 type ApiService interface {
 	GetOffices(ctx context.Context, filter OfficesFilter) (*Offices, error)
+	GetCities(ctx context.Context, filter CitiesFilter) (*[]City, error)
+	Calculator(ctx context.Context, request *CalculatorRequest) (*CalculatorResult, error)
 }
